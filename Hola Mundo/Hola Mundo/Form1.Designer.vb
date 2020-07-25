7e8f45891e36876b3e0f16e41d1a012c6581e046
Marvin Solorzano <marvinjosuesolorzano178@outlook.com>
2020-07-24 20:45:02 -0600

    Creacion del label bienvenida

--- Hola Mundo/Hola Mundo/Form1.Designer.vb
+++ Hola Mundo/Hola Mundo/Form1.Designer.vb
@@ -23,22 +23,33 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(274, 134)
+        Me.Label1.Location = New System.Drawing.Point(248, 101)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(80, 13)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "HOLA MUNDO"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(189, 159)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(190, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "BIENVENIDOS A PROGRAMACION 1"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(649, 321)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Name = "Form1"
         Me.Text = "Hola Mundo"
@@ -48,4 +59,5 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
 End Class
