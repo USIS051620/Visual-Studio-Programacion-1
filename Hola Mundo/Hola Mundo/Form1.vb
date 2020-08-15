d0cf12fdc6c16dc35c610debd16852ff158ef1a5
Marvin Solorzano <marvinjosuesolorzano178@outlook.com>
2020-08-15 11:43:18 -0600

    Se quito el limite de tabla hasta 12 e implemento el ciclo Do While (Hacer Mientras)

--- Hola Mundo/Hola Mundo/Form1.vb
+++ Hola Mundo/Hola Mundo/Form1.vb
@@ -1,15 +1,14 @@
 ﻿Public Class Form1
     Private Sub btnProcesar_Click(sender As Object, e As EventArgs) Handles btnProcesar.Click
-        Dim ntabla As SByte
+        Dim ntabla, i As Int64
         ntabla = txtntabla.Text
+        i = 1
 
         lstTablas.Items.Clear()
-        If ntabla <= 12 Then
-            For i = 1 To 10
-                lstTablas.Items.Add(ntabla.ToString() + “x” + i.ToString() + “=” + (ntabla * i).ToString())
-            Next
-        Else
-            lstTablas.Items.Add(“Por favor ingrese solamante tablas menores o iguales a 12.”)
-        End If
+        While i <= 10
+            lstTablas.Items.Add(ntabla.ToString() + “x” + i.ToString() + “=” + (ntabla * i).ToString())
+            i = i + 1
+        End While
+
     End Sub
 End Class
