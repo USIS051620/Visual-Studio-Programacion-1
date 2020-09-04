19122eb2c54897837d57709440e1a2d42003a5a8
Marvin Solorzano <marvinjosuesolorzano178@outlook.com>
2020-09-04 17:57:47 -0600

    Examen Parcial_1 terminado

--- Hola Mundo/Hola Mundo/Form1.Designer.vb
+++ Hola Mundo/Hola Mundo/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,65 +20,151 @@ Partial Class Form1
     'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
     'Se puede modificar usando el Diseñador de Windows Forms.  
     'No lo modifique con el editor de código.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.lblntabla = New System.Windows.Forms.Label()
-        Me.txtntabla = New System.Windows.Forms.TextBox()
-        Me.lstTablas = New System.Windows.Forms.ListBox()
-        Me.btnProcesar = New System.Windows.Forms.Button()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.btnBorrar = New System.Windows.Forms.Button()
+        Me.btnConvertir = New System.Windows.Forms.Button()
+        Me.txtRespuesta = New System.Windows.Forms.TextBox()
+        Me.lblRespuesta = New System.Windows.Forms.Label()
+        Me.txtUnidades = New System.Windows.Forms.TextBox()
+        Me.lblUnidades = New System.Windows.Forms.Label()
+        Me.txtCantidad = New System.Windows.Forms.TextBox()
+        Me.lblCantidad = New System.Windows.Forms.Label()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
         Me.SuspendLayout()
         '
-        'lblntabla
+        'TabControl1
         '
-        Me.lblntabla.AutoSize = True
-        Me.lblntabla.Location = New System.Drawing.Point(71, 127)
-        Me.lblntabla.Name = "lblntabla"
-        Me.lblntabla.Size = New System.Drawing.Size(37, 13)
-        Me.lblntabla.TabIndex = 0
-        Me.lblntabla.Text = "Tabla:"
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Location = New System.Drawing.Point(46, 31)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(575, 335)
+        Me.TabControl1.TabIndex = 0
         '
-        'txtntabla
+        'TabPage1
         '
-        Me.txtntabla.Location = New System.Drawing.Point(125, 127)
-        Me.txtntabla.Name = "txtntabla"
-        Me.txtntabla.Size = New System.Drawing.Size(84, 20)
-        Me.txtntabla.TabIndex = 1
+        Me.TabPage1.Controls.Add(Me.btnBorrar)
+        Me.TabPage1.Controls.Add(Me.btnConvertir)
+        Me.TabPage1.Controls.Add(Me.txtRespuesta)
+        Me.TabPage1.Controls.Add(Me.lblRespuesta)
+        Me.TabPage1.Controls.Add(Me.txtUnidades)
+        Me.TabPage1.Controls.Add(Me.lblUnidades)
+        Me.TabPage1.Controls.Add(Me.txtCantidad)
+        Me.TabPage1.Controls.Add(Me.lblCantidad)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(567, 309)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Universal"
+        Me.TabPage1.UseVisualStyleBackColor = True
         '
-        'lstTablas
+        'btnBorrar
         '
-        Me.lstTablas.FormattingEnabled = True
-        Me.lstTablas.Location = New System.Drawing.Point(233, 67)
-        Me.lstTablas.Name = "lstTablas"
-        Me.lstTablas.Size = New System.Drawing.Size(317, 147)
-        Me.lstTablas.TabIndex = 2
+        Me.btnBorrar.Location = New System.Drawing.Point(255, 187)
+        Me.btnBorrar.Name = "btnBorrar"
+        Me.btnBorrar.Size = New System.Drawing.Size(142, 65)
+        Me.btnBorrar.TabIndex = 7
+        Me.btnBorrar.Text = "Borrar"
+        Me.btnBorrar.UseVisualStyleBackColor = True
         '
-        'btnProcesar
+        'btnConvertir
         '
-        Me.btnProcesar.Location = New System.Drawing.Point(74, 169)
-        Me.btnProcesar.Name = "btnProcesar"
-        Me.btnProcesar.Size = New System.Drawing.Size(135, 45)
-        Me.btnProcesar.TabIndex = 3
-        Me.btnProcesar.Text = "Generar Tabla"
-        Me.btnProcesar.UseVisualStyleBackColor = True
+        Me.btnConvertir.Location = New System.Drawing.Point(40, 187)
+        Me.btnConvertir.Name = "btnConvertir"
+        Me.btnConvertir.Size = New System.Drawing.Size(142, 65)
+        Me.btnConvertir.TabIndex = 6
+        Me.btnConvertir.Text = "Convertir"
+        Me.btnConvertir.UseVisualStyleBackColor = True
+        '
+        'txtRespuesta
+        '
+        Me.txtRespuesta.Location = New System.Drawing.Point(104, 107)
+        Me.txtRespuesta.Name = "txtRespuesta"
+        Me.txtRespuesta.Size = New System.Drawing.Size(100, 20)
+        Me.txtRespuesta.TabIndex = 5
+        '
+        'lblRespuesta
+        '
+        Me.lblRespuesta.AutoSize = True
+        Me.lblRespuesta.Location = New System.Drawing.Point(37, 110)
+        Me.lblRespuesta.Name = "lblRespuesta"
+        Me.lblRespuesta.Size = New System.Drawing.Size(61, 13)
+        Me.lblRespuesta.TabIndex = 4
+        Me.lblRespuesta.Text = "Respuesta:"
+        '
+        'txtUnidades
+        '
+        Me.txtUnidades.Location = New System.Drawing.Point(313, 44)
+        Me.txtUnidades.Name = "txtUnidades"
+        Me.txtUnidades.Size = New System.Drawing.Size(100, 20)
+        Me.txtUnidades.TabIndex = 3
+        '
+        'lblUnidades
+        '
+        Me.lblUnidades.AutoSize = True
+        Me.lblUnidades.Location = New System.Drawing.Point(252, 47)
+        Me.lblUnidades.Name = "lblUnidades"
+        Me.lblUnidades.Size = New System.Drawing.Size(55, 13)
+        Me.lblUnidades.TabIndex = 2
+        Me.lblUnidades.Text = "Unidades:"
+        '
+        'txtCantidad
+        '
+        Me.txtCantidad.Location = New System.Drawing.Point(95, 44)
+        Me.txtCantidad.Name = "txtCantidad"
+        Me.txtCantidad.Size = New System.Drawing.Size(100, 20)
+        Me.txtCantidad.TabIndex = 1
+        '
+        'lblCantidad
+        '
+        Me.lblCantidad.AutoSize = True
+        Me.lblCantidad.Location = New System.Drawing.Point(37, 47)
+        Me.lblCantidad.Name = "lblCantidad"
+        Me.lblCantidad.Size = New System.Drawing.Size(52, 13)
+        Me.lblCantidad.TabIndex = 0
+        Me.lblCantidad.Text = "Cantidad:"
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(567, 309)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Area (Superficie)"
+        Me.TabPage2.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(649, 321)
-        Me.Controls.Add(Me.btnProcesar)
-        Me.Controls.Add(Me.lstTablas)
-        Me.Controls.Add(Me.txtntabla)
-        Me.Controls.Add(Me.lblntabla)
+        Me.ClientSize = New System.Drawing.Size(656, 413)
+        Me.Controls.Add(Me.TabControl1)
         Me.Name = "Form1"
-        Me.Text = "Tablas de Multiplicar"
+        Me.Text = "Parcial_1"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents lblntabla As Label
-    Friend WithEvents txtntabla As TextBox
-    Friend WithEvents lstTablas As ListBox
-    Friend WithEvents btnProcesar As Button
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents btnConvertir As Button
+    Friend WithEvents txtRespuesta As TextBox
+    Friend WithEvents lblRespuesta As Label
+    Friend WithEvents txtUnidades As TextBox
+    Friend WithEvents lblUnidades As Label
+    Friend WithEvents txtCantidad As TextBox
+    Friend WithEvents lblCantidad As Label
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents btnBorrar As Button
 End Class
--- Hola Mundo/Hola Mundo/Form1.vb
+++ Hola Mundo/Hola Mundo/Form1.vb
@@ -1,14 +1,42 @@
 ﻿Public Class Form1
-    Private Sub btnProcesar_Click(sender As Object, e As EventArgs) Handles btnProcesar.Click
-        Dim ntabla, i As Int64
-        ntabla = txtntabla.Text
-        i = 1
+    Private Sub btnConvertir_Click(sender As Object, e As EventArgs) Handles btnConvertir.Click
+        Dim Cantidad = txtCantidad.Text
+        Dim Unidades = txtUnidades.Text
+        Dim Respuesta = txtRespuesta.Text
+        Dim s, nr, sb, bc, pp, rp, nt, d, ro, z
 
-        lstTablas.Items.Clear()
-        While i <= 10
-            lstTablas.Items.Add(ntabla.ToString() + “x” + i.ToString() + “=” + (ntabla * i).ToString())
-            i = i + 1
-        End While
+        If txtRespuesta.Text = "" Then
+            rp = (Cantidad / Unidades)
+            nr = Int(rp)
+            sb = Cantidad - (nr * Unidades)
+            txtRespuesta.Text = nr.ToString() + "/" + sb.ToString()
+        End If
 
+        If txtCantidad.Text = "" Then
+            bc = ""
+            z = 0
+            nt = txtRespuesta.Text.Length
+
+            While z < nt
+                bc = txtRespuesta.Text.Substring(z, 1)
+                If bc = "/" Then
+                    d = z
+                End If
+                z = z + 1
+            End While
+
+            s = txtRespuesta.Text.Substring(0, d)
+            nt = (nt - d) - 1
+            d = d + 1
+            pp = txtRespuesta.Text.Substring(d, nt)
+            ro = (s * Unidades) + pp
+            txtCantidad.Text = ro
+        End If
+    End Sub
+
+    Private Sub btnBorrar_Click(sender As Object, e As EventArgs) Handles btnBorrar.Click
+        txtCantidad.Clear()
+        txtUnidades.Clear()
+        txtRespuesta.Clear()
     End Sub
 End Class
